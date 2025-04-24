c413dac30fabb65bd822e604e2be086a2dbcee9c
정찬욱 <jeongchan-ug@jeongchan-ug-ui-MacBookPro.local>
2025-04-24 19:01:11 +0900

    feat: preload를renderer로 연결하는 객체 추가

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const { app, BrowserWindow } = require("electron/main");
+import { app, BrowserWindow } from "electron/main";
+import { join } from "node:path";
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
